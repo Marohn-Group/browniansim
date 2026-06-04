fb202769a8c9daf4714e38ff6b8d7438689fad25
SKeeTElor <zw748@cornell.edu>
2026-06-03 23:32:21 -0400

    browniansim
    I developed a package using RK4 method to simulate the 1D oscillator's brownian motion

---
+++ browniansim/simulator.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+
+def harmosc_shiftw(X, Q, F,dw):
+    """
+    Damped harmonic oscillator with random driving force. Note: this differential equation treats time as time*w0, displacement as x*w0, and momentum as velocity.
+    Parameters:
+        X[array]: displacement, velocity.
+        Q[float]: quality factor.
+        F[array]: force devided by mass and w0.
+        x[float]: displacement time w0.
+        p[float]: velocity.
+        dotx[float]: change of displacement time w0.
+        dotp[float]: change of velocity.
+    Return:
+        dotx, dotp[array]: change of displacement*w0 and velocity.
+    """
+    x, p = X
+    dotx = p
+    dotp = - (1+dw)**2 * x - (1 / Q) * p + F
+    return np.array([dotx, dotp])
+
+def solve_RK4(X0, dtau, Q, F,dw):
+    """
+    Solve the damped harmonic oscillator equation using the Runge-Kutta 4th order method.
+    Parameters:
+        x[float]: displacement time w0.
+        p[float]: velocity.
+        dtau[float]: time step*w0.
+        Q[float]: quality factor.
+        F[array[n,4]]: force devided by the mass.
+    Return:
+        X[array]: displacement time w0, velocity.
+    """
+    X = np.zeros([np.shape(F)[0], 2])  # F is the force time-series. For the Euler method, each result corresponds to one F. For RK4, we need to use four identical F values for each step!
+    X[0] = X0
+    for n in range(np.shape(F)[0]-1):
+        k1 = harmosc_shiftw(X[n], Q, F[n][0],dw[n])
+        k2 = harmosc_shiftw(X[n] + dtau * k1 / 2, Q, F[n][1],dw[n])
+        k3 = harmosc_shiftw(X[n] + dtau * k2 / 2, Q, F[n][2],dw[n])
+        k4 = harmosc_shiftw(X[n] + dtau * k3, Q, F[n][3],dw[n])
+        X[n+1, :] =  X[n, :] + dtau * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+    return X
+
+
+def brownian_simulator(T, k, Q, dtau, X0, dw, std_shotN, withShotNoise=True):
+    r"""
+    Parameters:
+    T[float]: temperature(K)
+    k[float]: spring constant(N/m)
+    Q[float]: quality factor
+    f0[float]: resonant frequency(Hz)
+    dtau[float]: normalized time step: dt/w0. Time is normalized to the period of the resonator.
+    X0[list[2]]: initial position [x0/w0,v0], in which x0 is the initial position and v0 is the initial velocity, w0 is the resonant frequency
+    dw[np.array[N]]: frequency shift array, the real frequency is w0*(1+dw), so dw =angular velocity shift/w0; corresponding to freq shift in the time domain. Length of dw determine the measurement time.
+    std_shotN[float]: standard deviation of the shot noise(m)
+    withShotNoise[bool]: whether to add shot noise
+
+    Functions:
+    This function is used to simulate the brownian motion of a cantilever.
+
+    Returns:
+        noise[np.array]: noise array[N,2], in which the first column is the position and the second column is the velocity
+    """
+
+    kb = 1.38 * 10**-23  # boltzman constant
+    std_F = np.sqrt(
+        4 * kb * T / (Q * k * dtau)
+    )  # the standard deviation of the thermal force
+    F_row = np.random.normal(0, std_F, size=(len(dw), 1))
+    F = np.tile(F_row, (1, 4))
+    noise_eq = solve_RK4(X0, dtau, Q, F, dw)
+    if withShotNoise:
+        noise_shot = np.random.normal(0, std_shotN, size=np.shape(noise_eq))
+        noise = noise_eq + noise_shot
+    else:
+        noise = noise_eq
+    return noise
+
+
+def analytical_sol_ringdown(X0,Q,dw,dtau,p0=0):
+    """
+    Analytical solution of the damped harmonic oscillator equation.
+    Parameters:
+    X0[array]: initial amplitude, x*w0.
+    Q[float]: quality factor.
+    dw[array]: frequency shift vs time, shape (N,), delta w/w0.
+    dtau[float]: time step, notice that the period is 2*pi.
+    p0[float]: initial phase.
+    Returns:
+    t[array]: time array, shape (N,).
+    x[array]: displacement array, shape (N,).
+    """
+    phase = np.cumsum(dtau*(1+dw))+p0
+    phase = np.concatenate([np.array([p0]),phase[:-1]])
+    t = np.arange(0, len(dw) * dtau, dtau)
+    x = X0[0] * np.exp(-t / Q / 2) * np.cos(phase * np.sqrt(-1 / Q**2 / 4 + 1)) + X0[1] * np.exp(-t / Q / 2) * np.sin(phase * np.sqrt(-1 / Q**2 / 4 + 1))
+    return np.vstack((t,x)).T
+
+
+def analytical_sol_cons(X0,Q,dw,dtau,p0=0):
+    """
+    Analytical solution of the non-damped harmonic oscillator equation.
+    Parameters:
+    X0[array]: initial amplitude, x*w0.
+    Q[float]: quality factor.
+    dw[array]: frequency shift vs time, shape (N,), delta w/w0.
+    dtau[float]: time step, notice that the period is 2*pi.
+    p0[float]: initial phase.
+    Returns:
+    t[array]: time array, shape (N,).
+    x[array]: displacement array, shape (N,).
+    """
+    phase = np.cumsum(dtau*(1+dw))+p0
+    phase = np.concatenate([np.array([p0]),phase[:-1]])
+    t = np.arange(0, len(dw) * dtau, dtau)
+    x = X0[0] * np.cos(phase)+X0[1] * np.sin(phase)
+    return np.vstack((t,x)).T
+
+
+def pos2sig(pos,lam,V0,Vb,phi0=-np.pi/2):
+    """
+    Convert position to signal. We use interferometry to measure displacement.
+    pos[array]: position array, shape (N,).
+    lam[float]: wavelength.
+    V0[float]: amplitude of the signal.
+    Vb[float]: baseline of the signal.
+    phi0[float]: initial phase of the signal.
+    Returns:
+    sig[array]: signal array, shape (N,).
+    """
+    return V0*np.cos(2*np.pi*pos/lam+phi0)+Vb
---
+++ tests/test_simulator.py
@@ -0,0 +1,95 @@
+import numpy as np
+import browniansim as fc
+import pytest
+
+
+def _browniana_vs_RK4(Q):
+    dtau = 0.05*2*np.pi #our sampling rate is about 200kHz, w0 is about 8k, so about 20 times bigger
+    dw = np.array([]) # we need to set the efficient rad freq for eacxh set, w_eff = 1+dw, and real rad freq w(t) = w0*w_eff = w0*(1+dw)
+    k = 2.7*10**-3
+    kb = 1.38*10**-23
+    T = 0 # setting temp to be 0k, so there is no thermal jitter
+    std_shotN = 0
+
+    n = 50.3#how many cycle in each pulse
+    X0_0 = [1,0]# initial displacement and velocity for simulation
+    p0 = np.pi/2
+
+    dw_l = np.zeros(int(n*2*np.pi/dtau))#w = 1+dw
+
+    noise_browniana = fc.brownian_simulator(T, k, Q, dtau, X0_0, dw_l, std_shotN, withShotNoise=False)
+    noise_RK4 = fc.solve_RK4(X0_0, dtau, Q, np.zeros((len(dw_l),4)),dw_l)
+    return np.all(np.isclose(noise_browniana, noise_RK4, atol=1e-6))
+
+def test_browniana_is_smae_with_RK4():
+    """
+    Verify that the browniana simulator is the same as the RK4 simulator.
+    """
+    for _ in range(3):
+        Q = np.random.randint(100, 10000)
+        assert _browniana_vs_RK4(Q)
+
+
+
+def _browniana_vs_analytical(Q):
+    dtau = 0.05*2*np.pi #our sampling rate is about 200kHz, w0 is about 8k, so about 20 times bigger
+    dw = np.array([]) # we need to set the efficient rad freq for eacxh set, w_eff = 1+dw, and real rad freq w(t) = w0*w_eff = w0*(1+dw)
+
+    kb = 1.38*10**-23
+    k = 2.7*10**-3
+    T = 0 # setting temp to be 0k, so there is no thermal jitter
+    std_shotN = 0
+
+    n = 50.3#how many cycle in each pulse
+    X0_0 = [1,0]# initial displacement and velocity for simulation
+    p0 = 0
+
+    dw_l = np.zeros(int(n*2*np.pi/dtau))#w = 1+dw
+
+    noise_browniana = fc.brownian_simulator(T, k, Q, dtau, X0_0, dw_l, std_shotN, withShotNoise=False)[:,0]
+    noise_analytical = fc.analytical_sol_ringdown(X0_0,Q,dw_l,dtau,p0)[:,1]
+    return np.all(np.isclose(noise_browniana, noise_analytical, atol=5e-2))
+
+def test_browniana_is_close_to_analytical():
+    """
+    Verify that the browniana simulator is very close to the analytical solution.
+    """
+    for _ in range(3):
+        Q = np.random.randint(1000, 10000)
+        assert _browniana_vs_analytical(Q)
+
+
+def test_heatingup_signal():
+    """
+    Verify that the heating up signal has the correct expected value from the analytical solution.
+    """
+    Q = 18000
+    dtau = 0.05 * 2 * np.pi  # Our sampling rate is about 200kHz, w0 is about 8k, so about 20 times larger
+    dw = np.array([])  # We need to set the effective radial frequency for each set, w_eff = 1+dw, and the real radial frequency w(t) = w0*w_eff = w0*(1+dw)
+
+    kb = 1.38 * 10**-23
+    T = 275  # Set temperature to 0K, so there is no thermal jitter
+    k = 2.7 * 10**-3
+    w0 = 2 * np.pi * 8169.7
+    std_shotN = 0.1*10**-9
+
+    n = 50.3  # Number of cycles in each pulse
+    X0_0 = [1, 0]  # Initial displacement and velocity for simulation
+    p0 = np.pi / 2
+
+    dw_l = np.zeros(int(n * 2 * np.pi / dtau))  # w = 1+dw
+    N = 128
+    noise_negon_sqm = 0
+
+    sig_analytical = fc.analytical_sol_ringdown(X0_0, Q, dw_l, dtau, p0)  # While generating the analytical trajectory, we also generate the correct time series
+    sig_analytical[:, 0] = sig_analytical[:, 0] / w0
+    t = sig_analytical[:, 0]
+
+    for i in range(N):
+        X0_0 = [0,0]
+        noise_negon = fc.brownian_simulator(T, k, Q, dtau, X0_0, dw_l, std_shotN, withShotNoise=False)
+        noise_negon_sqm += noise_negon[:,0]**2/N
+
+    y_negon = kb * T*2 / k  * (1 - np.exp(-t *w0/ Q)) 
+
+    assert np.all(np.isclose(noise_negon_sqm, y_negon, rtol=5e-2))
